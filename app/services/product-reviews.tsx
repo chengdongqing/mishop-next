38468969331aadd38e5b9d0a2e59abaed5a00276
James Lu <1912525497@qq.com>
2025-04-08 12:00:30 +0800

    feat:完善评价统计计算

--- app/services/product-reviews.tsx
+++ app/services/product-reviews.tsx
@@ -1,30 +1,47 @@
 import { db } from '@/app/lib/db';
 import { productReviews } from '@/app/lib/schema';
-import { eq, sql } from 'drizzle-orm';
+import { and, eq, gte, sql } from 'drizzle-orm';
 
 export async function countReviewsByRatings(productId: number) {
-  // 总数
+  // 评价总数
   const totalCount = await db.$count(
     productReviews,
     eq(productReviews.productId, productId)
   );
 
-  const ratingCountRes =
-    await db.execute(sql`select ${productReviews.rating}, count(*) as value
-                         from ${productReviews}
-                         where ${productReviews.productId} = ${productId}
-                         group by ${productReviews.rating}`);
-  const ratingCountArray = ratingCountRes[0] as unknown as {
-    rating: number;
-    value: number;
-  }[];
-  const map = ratingCountArray.reduce((acc: Record<number, number>, curr) => {
-    acc[curr.rating] = curr.value;
-    return acc;
-  }, {});
+  // 每种评分的数量
+  const groupedRatings = await db
+    .select({
+      rating: productReviews.rating,
+      value: sql<number>`count(*)`
+    })
+    .from(productReviews)
+    .where(eq(productReviews.productId, productId))
+    .groupBy(productReviews.rating);
+  const ratingsMap = groupedRatings.reduce(
+    (acc: Record<number, number>, { rating, value }) => {
+      acc[rating] = value;
+      return acc;
+    },
+    {}
+  );
+
+  // 好评数量
+  const positiveCount = await db.$count(
+    productReviews,
+    and(eq(productReviews.productId, productId), gte(productReviews.rating, 3))
+  );
+
+  // 好评率
+  const positiveRate =
+    totalCount > 0
+      ? Math.floor((positiveCount / totalCount) * 10000) / 100
+      : 100;
 
   return {
     totalCount,
-    map
+    ratingsMap,
+    positiveCount,
+    positiveRate
   };
 }
