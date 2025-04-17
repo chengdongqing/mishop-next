5c5a2fe6fa4148f99a67002133a7f6404b8c0b69
James Lu <1912525497@qq.com>
2025-04-17 17:35:16 +0800

    feat:完善认证拦截

---
+++ middleware.ts
@@ -0,0 +1,16 @@
+import { auth } from '@/auth';
+
+const protectedRoutes = ['/cart'];
+
+export const middleware = auth((req) => {
+  const pathname = req.nextUrl.pathname;
+  const isProtected = protectedRoutes.some((path) => pathname.startsWith(path));
+
+  if (isProtected && !req.auth) {
+    return Response.redirect(new URL('/auth/signin', req.nextUrl.origin));
+  }
+});
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'] // 匹配所有的页面访问
+};
