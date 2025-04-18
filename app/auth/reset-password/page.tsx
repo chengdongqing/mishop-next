e1a198ae9f5093954f1d02f31042fbd38b7cfe85
James Lu <1912525497@qq.com>
2025-04-18 15:40:19 +0800

    feat:重置密码页面支持多语言

--- app/auth/reset-password/page.tsx
+++ app/auth/reset-password/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next';
+import { getTranslations } from 'next-intl/server';
 import ResetForm from './form';
 import TopBar from './top-bar';
 
@@ -6,7 +7,9 @@ export const metadata: Metadata = {
   title: '小米账号 - 重置密码'
 };
 
-export default function ResetPasswordPage() {
+export default async function ResetPasswordPage() {
+  const t = await getTranslations('ResetPassword');
+
   return (
     <div className={'min-h-screen min-w-screen dark:bg-black'}>
       <div className={'mx-auto flex min-h-screen w-[956] flex-col'}>
@@ -16,7 +19,7 @@ export default function ResetPasswordPage() {
             'mb-4 pt-3 text-center text-xl dark:text-[hsla(0,0%,100%,.95)]'
           }
         >
-          重置密码
+          {t('title')}
         </h4>
         <div className={'flex flex-1 items-center justify-center'}>
           <div
